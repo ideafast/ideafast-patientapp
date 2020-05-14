4787bd1a30ba396dcaeb53f53e82f3ef0ca1061a
Drew Miley <drewmiley92@gmail.com>
2020-05-14 12:37:57 +0100

    Implements bottom tab navigation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import {createStackNavigator} from '@react-navigation/stack';
 import {Provider} from 'react-redux';
 
 import Away from './containers/Away';
@@ -14,7 +15,8 @@ import Home from './containers/Home';
 import One from './containers/One';
 import configureStore from './configureStore';
 
-const {Navigator, Screen} = createStackNavigator();
+const {Navigator, Screen} = createBottomTabNavigator();
+// const {Navigator, Screen} = createStackNavigator();
 
 const store = configureStore();
 
