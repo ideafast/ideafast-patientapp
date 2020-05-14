12dcc3cb6cd60d2e3be7adacf4af76efb3a31346
Drew Miley <drewmiley92@gmail.com>
2020-05-14 13:38:13 +0100

    Nests Tab and Stack navigation

--- src/App.js
+++ src/App.js
@@ -2,33 +2,43 @@
  * @format
  * @flow strict-local
  */
-
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import {Provider} from 'react-redux';
 
-import Away from './containers/Away';
+// import Intro from './containers/Intro';
 import Home from './containers/Home';
+import Away from './containers/Away';
 import One from './containers/One';
+import Two from './containers/Two';
+import Three from './containers/Three';
 import configureStore from './configureStore';
 
-const {Navigator, Screen} = createBottomTabNavigator();
-// const {Navigator, Screen} = createStackNavigator();
+const {Navigator: TabNavigator, Screen: TabScreen} = createBottomTabNavigator();
+const {Navigator: StackNavigator, Screen: StackScreen} = createStackNavigator();
 
 const store = configureStore();
+
+const OneStack = () => (
+  <StackNavigator>
+    <StackScreen name="One" component={One} />
+    <StackScreen name="Two" component={Two} />
+    <StackScreen name="Three" component={Three} />
+  </StackNavigator>
+);
 
 const App: () => React$Node = () => {
   return (
     <NavigationContainer>
       <Provider store={store}>
-        <Navigator initialRouteName="One">
-          <Screen name="Away" component={Away} />
-          <Screen name="Home" component={Home} />
-          <Screen name="One" component={One} />
-        </Navigator>
+        <TabNavigator initialRouteName="Home">
+          <TabScreen name="Home" component={Home} />
+          <TabScreen name="Away" component={Away} />
+          <TabScreen name="One" component={OneStack} />
+        </TabNavigator>
       </Provider>
     </NavigationContainer>
   );
