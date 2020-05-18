76c27359206c29511871f080cb855700f4184f85
Farzaneh Farhadi <farzanehfarhadi@Farzanehs-MacBook-Air.local>
2020-05-18 12:00:40 +0100

    Added initial icons for tabs

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import FAQ from './containers/FAQ';
 import ContactUs from './containers/ContactUs';
 import AboutDevices from './containers/AboutDevices';
 import configureStore from './configureStore';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 const {Navigator: TabNavigator, Screen: TabScreen} = createBottomTabNavigator();
 const {Navigator: StackNavigator, Screen: StackScreen} = createStackNavigator();
@@ -35,10 +37,34 @@ const App: () => React$Node = () => {
   return (
     <NavigationContainer>
       <Provider store={store}>
-        <TabNavigator initialRouteName="Devices">
-          <TabScreen name="Devices" component={Devices} />
-          <TabScreen name="Profile" component={Profile} />
-          <TabScreen name="Help" component={HelpStack} />
+        <TabNavigator initialRouteName="Devices" tabBarOptions={{ activeTintColor: '#e91e63',}}>
+          <TabScreen name="Devices" component={Devices}
+            options={{
+                tabBarLabel: 'Devices',
+                tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="bluetooth" color={color} size={size} />
+                ),
+            }}
+          />
+          <TabScreen name="Profile" component={Profile}
+            options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="account-arrow-right-outline" color={color} size={size} />
+                 ),
+            }}
+          />
+          <TabScreen name="Help" component={HelpStack}
+            options={{
+                tabBarLabel: 'Help',
+                tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="question" color={color} size={size} />
+                ),
+            }}
+
+
+
+           />
         </TabNavigator>
       </Provider>
     </NavigationContainer>
