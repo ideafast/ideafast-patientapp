2b29dcc7f422b7ac60a46cd08a3c44faed10a474
Drew Miley <drewmiley92@gmail.com>
2020-05-22 01:58:43 +0100

    Removes sign out button from Profile screen

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -4,17 +4,13 @@
  * <div>Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
  */
 import React from 'react';
-import {StyleSheet, View, Button, Image} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 import {Text} from 'react-native-elements';
 import {connect} from 'react-redux';
 
 import {mapDispatchToProps} from '../ducks/actions';
 
 const Profile: () => React$Node = props => {
-  const signOut = () => {
-    props.navigation.navigate('Verify');
-  };
-
   return (
     <View style={styles.container}>
       <View style={styles.content}>
@@ -23,9 +19,6 @@ const Profile: () => React$Node = props => {
       <Text h4 style={styles.idNumber} underlineColorAndroid="transparent">
         {props.userID}
       </Text>
-      <View style={styles.content}>
-        <Button title="Sign Out" color="#841584" onPress={signOut} />
-      </View>
     </View>
   );
 };
