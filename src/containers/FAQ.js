2c83e6267fb643bb8af814814b947b4995cf8ed6
Farzaneh Farhadi <farzanehfarhadi@farzanehs-air.home>
2020-05-25 15:16:40 +0100

    Added collapse and expand Animatable to the questions and answers

--- src/containers/FAQ.js
+++ src/containers/FAQ.js
@@ -1,146 +1,205 @@
-/**
- * @format
- * @flow strict-local
- */
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
+  Switch,
   ScrollView,
-  View,
+  StyleSheet,
   Text,
-  StatusBar,
+  View,
+  TouchableOpacity,
 } from 'react-native';
+//import Constants from 'expo-constants';
+import * as Animatable from 'react-native-animatable';
+import Collapsible from 'react-native-collapsible';
+import Accordion from 'react-native-collapsible/Accordion';
 
 import {
   Header,
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {connect} from 'react-redux';
+const Answer1 =
+  'No. The warning has been already reported: https://github.com/archriss/react-native-snap-carousel/issues/672'
+const Answer2 =
+  'No, The warning will be fixed soon, as Animatable is open source and they are working on the problem';
 
-import {mapDispatchToProps} from '../ducks/actions';
+const Answer3 =
+  'Yes, I know, Do not worry, I am smart :|';
 
-const FAQ: () => React$Node = props => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Question1:</Text>
-              <Text style={styles.sectionDescription}>
-                This is <Text style={styles.highlight}>answer1</Text> lalla..
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Question2:</Text>
-              <Text style={styles.sectionDescription}>
-                This is <Text style={styles.highlight}>answer2</Text> lalla..
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Question3</Text>
-              <Text style={styles.sectionDescription}>
-                This is <Text style={styles.highlight}>answer3</Text> lalla..
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Question4:</Text>
-              <Text style={styles.sectionDescription}>
-                This is <Text style={styles.highlight}>answer4</Text> lalla..
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Question5:</Text>
-                <Text style={styles.sectionDescription}>
-                    This is <Text style={styles.highlight}>answer5</Text> lalla..
-                </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Question6:</Text>
-                <Text style={styles.sectionDescription}>
-                    This is <Text style={styles.highlight}>answer6</Text> lalla..
-                </Text>
-             </View>
-            <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Question7:</Text>
-                <Text style={styles.sectionDescription}>
-                    This is <Text style={styles.highlight}>answer7</Text> lalla..
-                </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Question8:</Text>
-                <Text style={styles.sectionDescription}>
-                    This is <Text style={styles.highlight}>answer8</Text> lalla..
-                </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Question9:</Text>
-                <Text style={styles.sectionDescription}>
-                    This is <Text style={styles.highlight}>answer9</Text> lalla..
-                </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Question10:</Text>
-                <Text style={styles.sectionDescription}>
-                    This is <Text style={styles.highlight}>answer10</Text> lalla..
-                    </Text>
-          </View>
-          </View>
+const Answer4 =
+  'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
+const Answer5 =
+  'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
+
+const CONTENT = [
+  {
+    title: 'Shall I report the warning on this page?',
+    content: Answer1,
+  },
+  {
+    title: 'Shall I worried about the warning on this page? ',
+    content: Answer2,
+  },
+  {
+    title: 'Do I know the rest of questions are fake and those are just for test?  ',
+    content: Answer3,
+  },
+  {
+    title: 'What sort of a device / application is this, what is it for and how does it work?',
+    content: Answer4,
+  },
+  {
+    title: 'Is it dangerous for my body if I use that?',
+    content: Answer5,
+  },
+  {
+    title: 'How long shall I keep that?',
+    content: Answer1,
+  },
+  {
+    title: 'What will need to be done before a patient can start using this?  ',
+    content: Answer1,
+  },
+  {
+    title: 'Question 8',
+    content: Answer1,
+  },
+  {
+    title: 'Question 9',
+    content: Answer1,
+  },
+  {
+    title: 'Question 10',
+     content: Answer1,
+  },
+
+];
+
+export default class App extends Component {
+  state = {
+    activeSections: [],
+    collapsed: true,
+    multipleSelect: false,
+  };
+
+  toggleExpanded = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
+  setSections = sections => {
+    this.setState({
+      activeSections: sections.includes(undefined) ? [] : sections,
+    });
+  };
+
+  renderHeader = (section, _, isActive) => {
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.header, isActive ? styles.active : styles.active]}
+        transition="backgroundColor"
+      >
+        <Text style={styles.headerText}>{section.title}</Text>
+      </Animatable.View>
+    );
+  };
+
+  renderContent(section, _, isActive) {
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.header, isActive ? styles.active : styles.active]}
+        transition="backgroundColor"
+      >
+        <Animatable.Text animation={isActive ? 'bounceIn' : undefined}>
+            <Text style={styles.content}>{section.content}</Text>
+        </Animatable.Text>
+      </Animatable.View>
+    );
+  }
+
+  render() {
+    const { multipleSelect, activeSections } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={{ paddingTop: 20 }}>
+          <Accordion
+            activeSections={activeSections}
+            sections={CONTENT}
+            renderHeader={this.renderHeader}
+            renderContent={this.renderContent}
+            duration={400}
+            onChange={this.setSections}
+          />
         </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    //marginTop: 2,
+    paddingHorizontal: 14,
+    //paddingTop: Constants.statusBarHeight,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  title: {
+    //textAlign: 'center',
+    fontSize: 30,
+    fontWeight: '600',
+    marginBottom: 20,
+    color: Colors.black,
   },
-  body: {
-    backgroundColor: Colors.white,
+  header: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
+  headerText: {
+    //textAlign: 'center',
+    fontSize: 20,
     fontWeight: '600',
     color: Colors.black,
   },
-  sectionDescription: {
+  content: {
+    //padding: 20,
     marginTop: 8,
+    backgroundColor: '#fff',
     fontSize: 18,
     fontWeight: '400',
     color: Colors.dark,
   },
-  highlight: {
-    fontWeight: '700',
+  active: {
+    backgroundColor: 'rgba(255,255,255,1)',
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
+  inactive: {
+    backgroundColor: 'rgba(245,252,255,1)',
+  },
+  selectors: {
+    marginBottom: 10,
+    flexDirection: 'row',
+  },
+  selector: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: 'bold',
+  },
+  selectTitle: {
+    fontSize: 20,
     fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    padding: 10,
+  },
+  multipleToggle: {
+    flexDirection: 'row',
+    //ustifyContent: 'center',
+    marginVertical: 30,
+    //alignItems: 'center',
+  },
+  multipleToggle__title: {
+    fontSize: 20,
+    marginRight: 8,
   },
 });
-
-const mapStateToProps = state => state;
-
-const FAQContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(FAQ);
-
-export default FAQContainer;
