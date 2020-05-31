9ffb547c423f30165b775274bfbfeba2454f84be
Farzaneh Farhadi <farzanehfarhadi@farzanehs-air.home>
2020-05-31 11:55:09 +0100

    Added placeholder for contact details

--- src/containers/ContactDetails.js
+++ src/containers/ContactDetails.js
@@ -3,23 +3,131 @@
  * @flow strict-local
  */
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
 import {connect} from 'react-redux';
 
 import {mapDispatchToProps} from '../ducks/actions';
 
 const ContactDetails: () => React$Node = props => {
   return (
-    <View style={styles.view}>
-      <Text>This is our contact information!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Newcastle, United Kingdom</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Person Name: </Text> Dr Alex
+                barbi
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Center Name: </Text> Urben
+                Science building
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Location: </Text> United Kingdom
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Newcastle, United Kingdom</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Person Name: </Text> Dr Alex
+                barbi
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Center Name: </Text> Urben
+                Science building
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Location: </Text> United Kingdom
+              </Text>
+            </View>
+
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Newcastle, United Kingdom</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Person Name: </Text> Dr Alex
+                barbi
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Center Name: </Text> Urben
+                Science building
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Location: </Text> United Kingdom
+              </Text>
+            </View>
+
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Newcastle, United Kingdom</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Person Name: </Text> Dr Alex
+                barbi
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Center Name: </Text> Urben
+                Science building
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>Location: </Text> United Kingdom
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    padding: 24,
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
 
