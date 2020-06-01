c60fc79b4aee57a168601fca1e270f3478080666
Drew Miley <drewmiley92@gmail.com>
2020-06-01 22:05:31 +0100

    Adds in initial action button

--- src/containers/Devices.js
+++ src/containers/Devices.js
@@ -4,6 +4,7 @@
  */
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import ActionButton from 'react-native-action-button';
 import {Text} from 'react-native-elements';
 import {connect} from 'react-redux';
 
@@ -14,11 +15,14 @@ const Devices: () => React$Node = props => {
     <View style={styles.view}>
       <Text h4>Connecting your first device</Text>
       <Text>
-        You are not connected to any devices. Tap the "+" button below to connect to all the devices you have.
+        You are not connected to any devices. Tap the "+" button below to
+        connect to all the devices you have.
       </Text>
       <Text>
-        If you would like more information about each device, tap the "Discover" button in the menu below.
+        If you would like more information about each device, tap the "Discover"
+        button in the menu below.
       </Text>
+      <ActionButton buttonColor="#5533FF" />
     </View>
   );
 };
