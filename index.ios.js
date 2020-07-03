c924138386553f2b765e8497a9344ae513a9d6ed
Farzaneh Farhadi <farzaneh.farhadi@newcastle.ac.uk>
2020-07-03 11:21:16 +0100

    Added bluetooth permission in AndroidManifest.js

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import React, { Component } from 'react';
+import {AppRegistry} from 'react-native';//
+import App from './src/App';//
+import {name as appName} from './app.json';
+//import App from "react-native-ble-manager/example/App";
+
+AppRegistry.registerComponent(appName, () => App);
