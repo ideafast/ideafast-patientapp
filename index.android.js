a6cccf4b89b0894ee0be11c6ed0e6d9ab7faf6d2
Farzaneh Farhadi <farzaneh.farhadi@newcastle.ac.uk>
2020-07-03 11:25:05 +0100

    removed component from index.xx.js

--- index.android.js
+++ index.android.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import React, { Component } from 'react';
+//import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
--- index.ios.js
+++ index.ios.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import React, { Component } from 'react';
-import {AppRegistry} from 'react-native';//
-import App from './src/App';//
+//import React, {Component} from 'react';
+import {AppRegistry} from 'react-native'; //
+import App from './src/App'; //
 import {name as appName} from './app.json';
 //import App from "react-native-ble-manager/example/App";
 
