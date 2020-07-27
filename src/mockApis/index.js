3e07fadbafbc1b4a140aa23b456948e0166b0999
Drew Miley <drewmiley92@gmail.com>
2020-07-27 11:52:41 +0100

    Initial returns of mock api data

--- src/mockApis/index.js
+++ src/mockApis/index.js
@@ -1,13 +1,38 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || '8000';
+// eslint-disable-next-line no-undef
+const rootDir = path.dirname(__filename);
 
-app.get('/', (req, res) => {
-  res.status(200).send('TEST');
+app.use(bodyParser.json());
+
+const fileread = filename => fs.readFileSync(filename);
+
+const router = express.Router();
+
+router.get('/devices', (req, res) => {
+  const data = fileread(`${rootDir}/devices.json`);
+  const formattedData = data;
+  res.status(200).json(formattedData.toString());
 });
 
+router.get('/deviceId/metrics', (req, res) => {
+  const data = fileread(`${rootDir}/metrics.json`);
+  const formattedData = data;
+  res.status(200).json(formattedData.toString());
+});
+
+router.get('/deviceId/status', (req, res) => {
+  const data = fileread(`${rootDir}/status.json`);
+  const formattedData = data;
+  res.status(200).json(formattedData.toString());
+});
+
+app.use('/api', router);
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
