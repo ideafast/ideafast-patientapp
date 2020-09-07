ff46d1b30d48271070a24913987fab90023ce450
Farzaneh Farhadi <farzaneh.farhadi@ncl.ac.uk>
2020-09-07 11:25:27 +0100

    cleaned color from black and white

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,3 +1,1 @@
 export const primary = '#c71585';
-export const white = '#ffffff';
-export const black = '#000000';
--- src/styles/typography.js
+++ src/styles/typography.js
@@ -14,19 +14,3 @@ export const FONT_SIZE_18 = scaleFont(18);
 export const FONT_SIZE_16 = scaleFont(16);
 export const FONT_SIZE_14 = scaleFont(14);
 export const FONT_SIZE_12 = scaleFont(12);
-
-// LINE HEIGHT
-export const LINE_HEIGHT_24 = scaleFont(24);
-export const LINE_HEIGHT_20 = scaleFont(20);
-export const LINE_HEIGHT_16 = scaleFont(16);
-
-// FONT STYLE
-export const FONT_REGULAR = {
-  fontFamily: FONT_FAMILY_REGULAR,
-  fontWeight: FONT_WEIGHT_REGULAR,
-};
-
-export const FONT_BOLD = {
-  fontFamily: FONT_FAMILY_BOLD,
-  fontWeight: FONT_WEIGHT_BOLD,
-};
