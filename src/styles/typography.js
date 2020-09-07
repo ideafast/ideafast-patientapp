5e339744382c0c54a5161181f9efdcfb506dcbca
Farzaneh Farhadi <farzaneh.farhadi@ncl.ac.uk>
2020-09-07 14:09:21 +0100

    deleted fonts family

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,9 +1,5 @@
 import {scaleFont} from './mixins';
 
-// FONT FAMILY
-export const FONT_FAMILY_REGULAR = 'OpenSans-Regular';
-export const FONT_FAMILY_BOLD = 'OpenSans-Bold';
-
 // FONT WEIGHT
 export const FONT_WEIGHT_REGULAR = '400';
 export const FONT_WEIGHT_BOLD = '700';
