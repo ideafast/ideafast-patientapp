b35138932bc3f3d1eaf22afe8ce230a0d17831a0
Jay Rainey <jawrainey@gmail.com>
2020-09-10 19:50:35 +0100

    Tidied Picker example
    - Removed unused views & Fixed lints

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,35 +1,27 @@
 import React, {useState} from 'react';
 import {connect} from 'react-redux';
-import {View, StyleSheet, Picker} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {Picker} from '@react-native-community/picker';
 
 import {Spacing, Colors} from '../styles';
 
 import {mapDispatchToProps} from '../ducks/actions';
 
 const DropDownMenu: () => React$Node = props => {
-  const filters = ['3 Days', '3 Months', '1 Week', '1 Day', 'All'];
+  const options = ['3 Days', '3 Months', '1 Week', '1 Day', 'All'];
 
-  const [option, setOption] = useState("");
+  const [option, setOption] = useState('');
 
   return (
-    <View>
-      <Picker
-        selectedValue={{option}}
-        style={styles.picker}
-        mode="dropdown"
-        onValueChange={(itemValue, itemIndex) => setOption(itemValue)}
-      >
-        {
-          filters.map((filter, item) => {
-            return (
-              <Picker.Item 
-                label={filter} 
-                value={filter}
-              />
-            );
-          })}
-      </Picker>
-    </View>
+    <Picker
+      selectedValue={option}
+      style={styles.picker}
+      mode="dropdown"
+      onValueChange={(itemValue, itemIndex) => setOption(itemValue)}>
+      {options.map((filter, index) => {
+        return <Picker.Item label={filter} value={index} key={index} />;
+      })}
+    </Picker>
   );
 };
 
@@ -50,4 +42,4 @@ const DropDownMenuContainer = connect(
   mapDispatchToProps,
 )(DropDownMenu);
 
-export default DropDownMenu;
+export default DropDownMenuContainer;
