6b0d8f230ad3487d664ad664a460ca6479cd5fe7
Farzaneh Farhadi <farzaneh.farhadi@ncl.ac.uk>
2020-09-23 14:59:24 +0100

    extracted data volumes from pie

---
+++ src/dataVisualization/index.js
@@ -0,0 +1,3 @@
+import * as Data from './DataVolume';
+
+export {Data};
--- src/visualization/DataVolume.js
+++ src/visualization/DataVolume.js
@@ -9,6 +9,7 @@ import {Colors, Typography, Spacing} from '../styles';
 import {connect} from 'react-redux';
 import {VictoryPie} from 'victory-native';
 import {Svg} from 'react-native-svg';
+import {Data} from '../dataVisualization';
 
 import {mapDispatchToProps} from '../ducks/actions';
 
@@ -16,22 +17,16 @@ const DataVolume: () => React$Node = props => {
   return (
     <View style={styles.view}>
       <Text style={styles.title}>Data Volume</Text>
-      <Svg width={300} height={300}>
+      <Svg>
         <VictoryPie
           standalone={false}
-          width={350}
-          height={220}
+          width={400}
+          height={230}
           innerRadius={35}
           labelPosition={({index}) => 'centroid'}
           padAngle={({datum}) => 2}
-          data={[
-            {x: '2GB', y: 10},
-            {x: '680MB', y: 32},
-            {x: '490MB', y: 30},
-            {x: '70MB', y: 15},
-            {x: '2KB', y: 3},
-          ]}
-          colorScale={['#5ba5ce', '#fe234f', '#595a5e', '#219ca3', '#d1fcff']}
+          data={Data.data}
+          colorScale={Data.colors}
         />
       </Svg>
     </View>
