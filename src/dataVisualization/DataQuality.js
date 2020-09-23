c61db85975ec3225f4194c4d8bfd5f2751ae00ed
Farzaneh Farhadi <farzaneh.farhadi@ncl.ac.uk>
2020-09-23 15:28:15 +0100

    added DataQuality chart

---
+++ src/dataVisualization/DataQuality.js
@@ -0,0 +1,9 @@
+export const row1 = [
+  {x: '2GB', y: 10},
+  {x: '680MB', y: 32},
+  {x: '490MB', y: 30},
+  {x: '70MB', y: 15},
+  {x: '2KB', y: 3},
+];
+
+export const colors = ['#5ba5ce', '#fe234f', '#595a5e', '#219ca3', '#d1fcff'];
--- src/visualization/DataQuality.js
+++ src/visualization/DataQuality.js
@@ -7,13 +7,36 @@ import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-elements';
 import {Colors, Typography, Spacing} from '../styles';
 import {connect} from 'react-redux';
+import {VictoryGroup, VictoryBar, VictoryChart} from 'victory-native';
 
 import {mapDispatchToProps} from '../ducks/actions';
 
 const DataQuality: () => React$Node = props => {
   return (
     <View style={styles.view}>
-      <Text style={styles.title}>Progress</Text>
+      <Text style={styles.title}>Data Quality</Text>
+      <View style={styles.victoryChart}>
+        <VictoryChart width={300} height={220}>
+          <VictoryGroup
+            offset={35}
+            colorScale={['#5ba5ce', '#fe234f', '#595a5e', '#219ca3', '#d1fcff']}
+            style={{
+              data: {
+                fillOpacity: 0.6,
+                stroke: 'black',
+                strokeWidth: 1,
+              },
+            }}>
+            <VictoryBar data={[{x: '11/07/20 to 13/07/20 (3 days)', y: 1}]} />
+            <VictoryBar data={[{x: '11/07/20 to 13/07/20 (3 days)', y: 0.8}]} />
+            <VictoryBar
+              data={[{x: '11/07/20 to 13/07/20 (3 days)', y: 0.75}]}
+            />
+            <VictoryBar data={[{x: '11/07/20 to 13/07/20 (3 days)', y: 1}]} />
+            <VictoryBar data={[{x: '11/07/20 to 13/07/20 (3 days)', y: 0.2}]} />
+          </VictoryGroup>
+        </VictoryChart>
+      </View>
     </View>
   );
 };
@@ -29,6 +52,10 @@ const styles = StyleSheet.create({
     color: Colors.BLACK,
     marginLeft: Spacing.SCALE_8,
     marginTop: Spacing.SCALE_8,
+  },
+  victoryChart: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
