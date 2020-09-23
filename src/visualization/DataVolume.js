9f935beb4510b7c9c334304c01772c8504f4b00f
Farzaneh Farhadi <farzaneh.farhadi@ncl.ac.uk>
2020-09-23 14:33:16 +0100

    Added data Volume

--- src/visualization/DataVolume.js
+++ src/visualization/DataVolume.js
@@ -7,6 +7,8 @@ import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-elements';
 import {Colors, Typography, Spacing} from '../styles';
 import {connect} from 'react-redux';
+import {VictoryPie} from 'victory-native';
+import {Svg} from 'react-native-svg';
 
 import {mapDispatchToProps} from '../ducks/actions';
 
@@ -14,6 +16,24 @@ const DataVolume: () => React$Node = props => {
   return (
     <View style={styles.view}>
       <Text style={styles.title}>Data Volume</Text>
+      <Svg width={300} height={300}>
+        <VictoryPie
+          standalone={false}
+          width={350}
+          height={220}
+          innerRadius={35}
+          labelPosition={({index}) => 'centroid'}
+          padAngle={({datum}) => 2}
+          data={[
+            {x: '2GB', y: 10},
+            {x: '680MB', y: 32},
+            {x: '490MB', y: 30},
+            {x: '70MB', y: 15},
+            {x: '2KB', y: 3},
+          ]}
+          colorScale={['#5ba5ce', '#fe234f', '#595a5e', '#219ca3', '#d1fcff']}
+        />
+      </Svg>
     </View>
   );
 };
