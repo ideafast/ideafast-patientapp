15397df1148cf9d3fd40986856672610b615365f
Жека Гу <b9019367@newcastle.ac.uk>
2020-09-28 13:36:16 +0300

    Restructured 'i18n' folder and added index.js file

---
+++ src/i18n/resources/index.js
@@ -0,0 +1,38 @@
+import ContributionsDE from './de/Contributions.json';
+import ContributionsEN from './en/Contributions.json';
+import ContributionsNL from './nl/Contributions.json';
+
+import DevicesDE from './de/Devices.json';
+import DevicesEN from './en/Devices.json';
+import DevicesNL from './nl/Devices.json';
+
+import LoginDE from './de/LogIn.json';
+import LoginEN from './en/LogIn.json';
+import LoginNL from './nl/LogIn.json';
+
+import SupportDE from './de/Support.json';
+import SupportEN from './en/Support.json';
+import SupportNL from './nl/Support.json';
+
+export default {
+  contributionsContainer: {
+    en: ContributionsEN,
+    de: ContributionsDE,
+    nl: ContributionsNL,
+  },
+  devicesContainer: {
+    en: DevicesEN,
+    de: DevicesDE,
+    nl: DevicesNL,
+  },
+  loginContainer: {
+    en: LoginEN,
+    de: LoginDE,
+    nl: LoginNL,
+  },
+  SupportContainer: {
+    en: SupportEN,
+    de: SupportDE,
+    nl: SupportNL,
+  },
+};
