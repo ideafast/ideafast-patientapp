c138f84e42cf9fff627f9d8c648edd07e023ca01
Farzaneh Farhadi <farzaneh.farhadi@ncl.ac.uk>
2020-09-30 17:05:37 +0100

    revised initiate state

--- src/ducks/initialState.js
+++ src/ducks/initialState.js
@@ -1,17 +1,54 @@
 const defaultImage = require('../assets/devices/default.png');
+import device_metrics from '../api/mock-data/devices.json';
+import {Colors} from '../styles';
 
 const DEVICES = [
-  {id: 0, name: 'Axivity', image: defaultImage},
-  {id: 1, name: 'Byteflies', image: defaultImage},
-  {id: 2, name: 'Dreem', image: defaultImage},
-  {id: 3, name: 'eBedSensor', image: defaultImage},
-  {id: 4, name: 'Everion', image: defaultImage},
-  {id: 5, name: 'Move Monitor', image: defaultImage},
-  {id: 6, name: 'Samsung Smartphone', image: defaultImage},
-  {id: 7, name: 'Stress Monitor', image: defaultImage},
-  {id: 8, name: 'Think Fast', image: defaultImage},
-  {id: 9, name: 'Vital Patch', image: defaultImage},
-  {id: 10, name: 'ZKOne YOLI', image: defaultImage},
+  {id: 0, name: 'Axivity', image: defaultImage, color: Colors.DEVICES.AX6},
+  {
+    id: 1,
+    name: 'Byteflies',
+    image: defaultImage,
+    color: Colors.DEVICES.BYTEFLIES,
+  },
+  {id: 2, name: 'Dreem', image: defaultImage, color: Colors.DEVICES.DREEM},
+  {id: 3, name: 'eBedSensor', image: defaultImage, color: Colors.DEVICES.null},
+  {id: 4, name: 'Everion', image: defaultImage, color: Colors.DEVICES.EVERION},
+  {
+    id: 5,
+    name: 'Move Monitor',
+    image: defaultImage,
+    color: Colors.DEVICES.null,
+  },
+  {
+    id: 6,
+    name: 'Samsung Smartphone',
+    image: defaultImage,
+    color: Colors.DEVICES.null,
+  },
+  {
+    id: 7,
+    name: 'Stress Monitor',
+    image: defaultImage,
+    color: Colors.DEVICES.null,
+  },
+  {
+    id: 8,
+    name: 'Think Fast',
+    image: defaultImage,
+    color: Colors.DEVICES.THINKFAST,
+  },
+  {
+    id: 9,
+    name: 'Vital Patch',
+    image: defaultImage,
+    color: Colors.DEVICES.null,
+  },
+  {
+    id: 10,
+    name: 'ZKOne YOLI',
+    image: defaultImage,
+    color: Colors.DEVICES.null,
+  },
 ];
 
 const SUPPORTED_LANGUAGES = [
@@ -34,4 +71,5 @@ export default {
   userLang: 'en',
   languages: SUPPORTED_LANGUAGES,
   devices: DEVICES,
+  deviceMetrics: device_metrics,
 };
--- src/visualization/DataVolume.js
+++ src/visualization/DataVolume.js
@@ -9,11 +9,14 @@ import {Colors, Typography, Spacing} from '../styles';
 import {connect} from 'react-redux';
 import {VictoryPie} from 'victory-native';
 import {Svg} from 'react-native-svg';
-import {DataPie} from '../dataVisualization';
 
 import {mapDispatchToProps} from '../ducks/actions';
 
 const DataVolume: () => React$Node = props => {
+  const deviceSizes = props.deviceMetrics.map(d =>
+    d.metrics.sessions.map(s => s.size).reduce((a, b) => a + b),
+  );
+
   return (
     <View style={[styles.view, styles.border]}>
       <Text style={styles.title}>Data Volume</Text>
@@ -26,8 +29,8 @@ const DataVolume: () => React$Node = props => {
             innerRadius={15}
             labelPosition={({index}) => 'centroid'}
             padAngle={({datum}) => 2}
-            data={DataPie.data}
-            colorScale={DataPie.Colors}
+            data={deviceSizes}
+            colorScale={props.devices.map(d => d.color)}
           />
         </Svg>
       </View>
