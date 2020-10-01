6c95a2fc736b3206a39e1db9915e2dae0f75d535
jevgenijguzikovskij <63454824+b9019367@users.noreply.github.com>
2020-10-01 16:19:41 +0300

    Added translation to ./stack/support.js

--- src/stacks/support.js
+++ src/stacks/support.js
@@ -1,23 +1,27 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import {Stack} from '../styles';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Support from '../containers/support/Support';
 import SupportDoc from '../containers/support/SupportDoc';
+import {useTranslation} from 'react-i18next';
 
 export const {
   Navigator: StackNavigator,
   Screen: StackScreen,
 } = createStackNavigator();
 
-export const SupportStack = () => (
-  <StackNavigator screenOptions={Stack.headerStyle}>
-    <StackScreen
-      name="Support"
-      component={Support}
-      options={{
-        title: 'Tap a device to learn more',
-      }}
-    />
-    <StackScreen name="SupportDoc" component={SupportDoc} />
-  </StackNavigator>
-);
+export const SupportStack = () => {
+  const {t, i18n} = useTranslation('support');
+  return (
+    <StackNavigator screenOptions={Stack.headerStyle}>
+      <StackScreen
+        name="Support"
+        component={Support}
+        options={{
+          title: t('text'),
+        }}
+      />
+      <StackScreen name="SupportDoc" component={SupportDoc} />
+    </StackNavigator>
+  );
+};
