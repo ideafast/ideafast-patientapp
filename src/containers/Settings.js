c9de1b78c6e7fe6b5681b8fb7b7e543cea14af84
jevgenijguzikovskij <63454824+b9019367@users.noreply.github.com>
2020-10-01 16:29:11 +0300

    Added tranaslation to ./containers/Settings.js .

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -7,13 +7,15 @@ import {View} from 'react-native';
 import {connect} from 'react-redux';
 import LanguageSelection from './LanguageSelection';
 import SettingsRow from '../components/SettingsRow';
+import {useTranslation} from 'react-i18next';
 
 import {mapDispatchToProps} from '../ducks/actions';
 
 const Settings: () => React$Node = props => {
+  const {t, i18n} = useTranslation('contributions');
   return (
     <View>
-      <SettingsRow title={'User ID'} children={props.userID} />
+      <SettingsRow title={t('settings.user')} children={props.userID} />
       <LanguageSelection />
     </View>
   );
