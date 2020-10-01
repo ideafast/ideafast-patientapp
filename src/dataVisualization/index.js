67e181a8d9df1afbbf23f4fa78e1a2b47f1fd138
Farzaneh Farhadi <farzaneh.farhadi@ncl.ac.uk>
2020-10-01 12:13:27 +0100

    fixed capital letters

--- src/dataVisualization/index.js
+++ src/dataVisualization/index.js
@@ -1,5 +1,5 @@
 import * as DataPie from './DataVolume';
 import * as DataChart from './DataQuality';
-import * as DataProgress from './Dataprogress';
+import * as DataProgress from './DataProgress';
 
 export {DataPie, DataChart, DataProgress};
