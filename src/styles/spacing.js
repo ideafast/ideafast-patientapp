841d27c5ab786337216cfcea09af65d68ca32fbe
Jay Rainey <jawrainey@gmail.com>
2020-10-09 16:25:58 +0100

    Remove unused spacing

--- src/styles/spacing.js
+++ src/styles/spacing.js
@@ -1,8 +1,5 @@
 import {scaleSize} from './mixins';
 
-export const SCALE_250 = scaleSize(250);
-export const SCALE_200 = scaleSize(200);
-export const SCALE_150 = scaleSize(150);
 export const SCALE_90 = scaleSize(90);
 export const SCALE_42 = scaleSize(42);
 export const SCALE_36 = scaleSize(36);
@@ -10,4 +7,3 @@ export const SCALE_24 = scaleSize(24);
 export const SCALE_16 = scaleSize(16);
 export const SCALE_8 = scaleSize(8);
 export const SCALE_4 = scaleSize(4);
-export const SCALE_1 = scaleSize(1);
