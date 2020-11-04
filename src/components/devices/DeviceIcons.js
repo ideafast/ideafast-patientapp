f4b3b09416cd4d6002f1b590a5edc2fe8c77b9f7
Jay Rainey <jawrainey@gmail.com>
2020-11-04 11:14:06 +0000

    Remove unnecessary if statement
    - Also means removing eslint comment and that this component logic is procedural

--- src/components/devices/DeviceIcons.js
+++ src/components/devices/DeviceIcons.js
@@ -17,44 +17,39 @@ const DeviceIcons: () => React$Node = ({status}) => {
   // TODO: push a notification to the user
   const pushNotfication = message => true;
 
-  /* eslint-disable no-shadow */
-  const iconsForStatus = status => {
-    const icons = [];
+  const icons = [];
 
-    if (status.features.ble) {
-      const isBLEOn = isDeviceConnectedToBLE(status.features.ble.uuid);
-      const isConnectedToBLE = isBLEOn ? 'bluetooth' : 'bluetooth-off';
+  if (status.features.ble) {
+    const isBLEOn = isDeviceConnectedToBLE(status.features.ble.uuid);
+    const isConnectedToBLE = isBLEOn ? 'bluetooth' : 'bluetooth-off';
 
-      // TODO: determine if usage is witin wear-time before pushing
-      if (!isBLEOn) {
-        pushNotfication('');
-      }
-
-      icons.push(chooseIcon(isConnectedToBLE));
+    // TODO: determine if usage is witin wear-time before pushing
+    if (!isBLEOn) {
+      pushNotfication('');
     }
 
-    if (status.connection.battery) {
-      const percent = status.connection.battery;
-      const batteryStatus = percent === 100 ? 'battery' : `battery-${percent}`;
-      icons.push(chooseIcon(batteryStatus));
-    }
+    icons.push(chooseIcon(isConnectedToBLE));
+  }
 
-    if (status.features.wired) {
-      const isWiredConnected = status.connection.wifi
-        ? 'power-plug-outline'
-        : 'power-plug-off-outline';
-      icons.push(chooseIcon(isWiredConnected));
-    }
+  if (status.connection.battery) {
+    const percent = status.connection.battery;
+    const batteryStatus = percent === 100 ? 'battery' : `battery-${percent}`;
+    icons.push(chooseIcon(batteryStatus));
+  }
 
-    if (status.features.wifi) {
-      const wifiIcon = status.connection.wifi ? 'wifi' : 'wifi-off';
-      icons.push(chooseIcon(wifiIcon));
-    }
+  if (status.features.wired) {
+    const isWiredConnected = status.connection.wifi
+      ? 'power-plug-outline'
+      : 'power-plug-off-outline';
+    icons.push(chooseIcon(isWiredConnected));
+  }
 
-    return icons;
-  };
+  if (status.features.wifi) {
+    const wifiIcon = status.connection.wifi ? 'wifi' : 'wifi-off';
+    icons.push(chooseIcon(wifiIcon));
+  }
 
-  return iconsForStatus(status);
+  return icons;
 };
 
 const styles = StyleSheet.create({
