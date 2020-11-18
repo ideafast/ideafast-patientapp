5339131a91c0eaf18a80f460eed2530cd6c37f30
Jay Rainey <jawrainey@gmail.com>
2020-11-18 14:31:33 +0000

    Minor edits and enabled clone_repo

--- src/util/BuildMd.py
+++ src/util/BuildMd.py
@@ -1,17 +1,9 @@
 import os
 import shutil
 import subprocess
-import stat
+import stat 
 from pathlib import Path
 
-# todo:
-#  1. Find alternative for os.walk in Pathlib package.
-#  2. Check whether 'stat' works on other OS platforms.
-#  3. Find out why cannot clone git repo from a script.
-
-# To use:
-# Clone (cloned by running 'git clone https://github.com/ideafast/ideafast-devicesupportdocs-web.git ./SupportDocumentation'
-# in the root repo).
 
 # Structure will be: { device: [ path1, path2,... ] }
 DOCS_PATH_STORE = {}
@@ -19,10 +11,10 @@ DOCS_PATH_STORE = {}
 DOCS_PATH = Path(r'../../SupportDocumentation')
 
 
-# returns
 def fill_keys(lang_extension: Path) -> dict:
     DOCS_PATH_STORE.clear()
-    return {f.name: [] for f in (DOCS_PATH / "_i18n" / lang_extension / "_common").glob('**/*') if f.is_dir()}
+    _files = (DOCS_PATH / "_i18n" / lang_extension / "_common").glob('**/*')
+    return {f.name: [] for f in _files if f.is_dir()}
 
 
 def fill_values(lang_extension: Path):
@@ -34,19 +26,20 @@ def fill_values(lang_extension: Path):
 
 def move_files(target_dir: Path):
     for dev, num in DOCS_PATH_STORE.items():
-        target_dir2 = target_dir / dev
-        if not target_dir2.resolve().is_dir():
-            target_dir2.mkdir()
-        else:
+        _target_dir = target_dir / dev
+        
+        if _target_dir.resolve().is_dir():
             shutil.rmtree(target_dir2.resolve())
-            target_dir2.mkdir()
+        _target_dir.mkdir()
+
         for device_path in num:
-            [shutil.copy(document_path.resolve(), target_dir2.resolve()) for document_path in device_path.iterdir()]
+            for document_path in device_path.iterdir():
+                shutil.copy(document_path.resolve(), _target_dir.resolve())
 
 
 def read_and_copy(lang_extension: Path):
     fill_values(lang_extension)
-    dir_path = Path(f'../i18n/docs/{lang}')
+    dir_path = Path(f'../i18n/docs/{lang_extension}')
     if dir_path.is_dir():
         shutil.rmtree(dir_path)
     dir_path.mkdir()
@@ -54,12 +47,13 @@ def read_and_copy(lang_extension: Path):
 
 
 def delete_repo():
-    for root, dirs, files in os.walk("../../SupportDocumentation"):
+    # note: this is only required for windows
+    for root, dirs, files in os.walk(DOCS_PATH):
         for directory in dirs:
             Path.chmod((Path(root) / directory), stat.S_IRWXU)
         for file in files:
             Path.chmod((Path(root) / file), stat.S_IRWXU)
-    shutil.rmtree('../../SupportDocumentation')
+    shutil.rmtree(DOCS_PATH)
 
 
 def copy_images():
@@ -72,19 +66,19 @@ def copy_images():
         shutil.copytree(image_directory.resolve(), _path.resolve())
 
 
-# def clone_repo():
-    # subprocess.call(['git clone git@github.com:ideafast/ideafast-devicesupportdocs-web.git '
-    #                  '../../SupportDocumentation'], shell=True)
-    # subprocess.Popen(['git', 'clone', 'git@github.com:ideafast/ideafast-devicesupportdocs-web.git',
-    #                  '/SupportDocumentation'])
-
-
-if __name__ == '__main__':
-    docs = Path('./docs/_i18n')
-    # clone_repo()
-
+def copy_markdown():
     for lang in [Path('en'), Path('de'), Path('nl')]:
         DOCS_PATH_STORE.update(fill_keys(lang))
         read_and_copy(lang)
+
+
+def clone_repo():
+    url = 'git@github.com:ideafast/ideafast-devicesupportdocs-web.git'
+    subprocess.call([f'git clone {url} {DOCS_PATH}'], shell=True)
+
+
+if __name__ == '__main__':
+    clone_repo()
+    copy_markdown()
     copy_images()
     delete_repo()
