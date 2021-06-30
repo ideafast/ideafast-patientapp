4e5cd0fc390cb12ddca8cdb9ef69dd2aadb8b3cb
Jay Rainey <jawrainey@gmail.com>
2021-06-30 12:03:11 +0100

    Added shell script to automatically build docs
    This allows us not to have the complete static website locally without being on git (as it's in another repo).

---
+++ android/app/src/main/assets/docs/main.sh
@@ -0,0 +1,61 @@
+# OBTAIN AND BUILD DOCS REPO FOR USE IN APP
+
+DOCS="../docs/"
+TMP_DOCS="./tmpdocs"
+STATIC_SITE="${TMP_DOCS}/_site/"
+
+# STEP 1: GET THE REPO
+rm -rf ${TMP_DOCS}
+
+git clone git@github.com:ideafast/ideafast-devicesupportdocs-web.git ${TMP_DOCS}
+
+# STEP 2: INSTALL DOCS LOCALLY
+
+# Needed on my (Jays) machine to build jekyll
+PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH
+
+cd ${TMP_DOCS}
+
+bundle install && bundle exec jekyll build
+
+cd ..
+
+# STEP 3:  MOVE RELEVANT DOCS INSIDE THE APP
+
+# Folders must exist before moving them
+mkdir -p ${DOCS}{en,nl,de,images,assets}
+
+# Copy files rather than move so we can re-run as needed.
+cp -r ${STATIC_SITE}pat/. ${DOCS}en/
+cp -r ${STATIC_SITE}nl/pat/. ${DOCS}nl/
+cp -r ${STATIC_SITE}de/pat/. ${DOCS}de/
+cp -r ${STATIC_SITE}images/. ${DOCS}images/
+cp -r ${STATIC_SITE}assets/. ${DOCS}assets/
+
+# STEP 4: REMOVE DOCS LOCALLY (PREVENT BLOAT)
+rm -rf ${TMP_DOCS}
+
+# STEP 5: REPLACE ABSOLUTE URLs FOR RELATIVE
+
+# NOTE: have to set extension to empty string on OSX as it's not GNU seD:
+# see https://stackoverflow.com/questions/39325759/sed-on-mac-extra-characters-after-p-command
+
+cd ${DOCS}de/
+sed -i '' 's,https://docs.ideafast.eu/de/assets/,../assets/,g' *
+sed -i '' 's,https://docs.ideafast.eu/de/,../,g' * 
+cd ..
+
+cd ${DOCS}nl/
+sed -i '' 's,https://docs.ideafast.eu/nl/assets/,../assets/,g' * 
+sed -i '' 's,https://docs.ideafast.eu/nl/,../,g' * 
+cd ..
+
+cd ${DOCS}en/
+sed -i '' 's,https://docs.ideafast.eu/assets/,../assets/,g' *
+sed -i '' 's,https://docs.ideafast.eu/,../,g' *
+cd ..
+
+# TODO: The following could be removed but for now does not impact app: 
+# 1. JavaScript service worker (its purpose is not for mobile)
+# 2. NAV tag (it's hidden on mobile via CSS)
